034690029d3c0e99235c8f45a0fb6c7465cebca3
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-04-07 02:46:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ START_TXT = """
 """
 
 START_BTN = InlineKeyboardMarkup(
-        [[InlineKeyboardButton('Add Channel', url='https://t.me/{}?startchannel=&admin=post_messages'.format(BOT_USERNAME))]]
-    )
+    [[InlineKeyboardButton('Add Channel', url='https://t.me/{}?startchannel=&admin=post_messages'.format(BOT_USERNAME))]]
+)
 
 @bot.on_message(filters.command(["start"]))
 async def start(bot, update):
@@ -31,18 +31,27 @@ async def start(bot, update):
         reply_markup=reply_markup
     )
 
+async def delete_message(chat, message_id):
+    try:
+        await bot.delete_messages(chat_id=chat, message_ids=message_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await delete_message(chat, message_id)
+    except Exception as e:
+        print(f"Error deleting message: {e}")
+
 @bot.on_message(filters.channel & filters.forwarded)
 async def fwdrmv(c, m):
     try:
         if m.media and not (m.video_note or m.sticker):
             new_caption = f"<b>{m.caption}</b>\n\n" if m.caption else None
-            await m.copy(m.chat.id, caption=new_caption)
-            await m.delete()
+            new_message = await m.copy(m.chat.id, caption=new_caption)
+            await delete_message(m.chat.id, m.message_id)
         else:
-            await m.copy(m.chat.id)
-            await m.delete()
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
+            new_message = await m.copy(m.chat.id)
+            await delete_message(m.chat.id, m.message_id)
+    except Exception as e:
+        print(f"Error processing message: {e}")
 
 @bot.on_message(filters.private | filters.group)
 async def fwdrm(c, m):
@@ -52,7 +61,7 @@ async def fwdrm(c, m):
             await m.copy(m.chat.id, caption=new_caption)
         else:
             await m.copy(m.chat.id)
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
+    except Exception as e:
+        print(f"Error processing message: {e}")
 
 bot.run()
